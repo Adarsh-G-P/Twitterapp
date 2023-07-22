411281a26be039ecc3acdb55811f557600404a07
Adarsh-G-P <adarshgp96@gmail.com>
2023-07-22 17:20:08 +0530

    bootstrap added with a base templates

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse, Http404
+from .models import tweet
 
 # Create your views here.
-def home_view (request,*args, **kwargs):
-    return HttpResponse("<h1> Hello Twitter</h1>")
+def home_view (request, *args, **kwargs):
+    # return HttpResponse("<h1> Hello Twitter</h1>")
+    return render (request, "pages/home.html", context={}, status = 200)
+
+def tweet_detail_view (request, tweet_id, *args, **kwargs):
+    data = {
+        "id" : tweet_id,   
+    }
+    status = 200 
+    try:
+        obj = tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+        
+
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    print("Data:", data)     
+
+    return JsonResponse(data, status = status) 
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>") 
